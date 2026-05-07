5fc8286cdc2011ffb5064086db72ce95c4e63082
AlbertoMMUE <alemarde@alumni.uv.es>
2026-05-07 20:05:39 +0200

    feat(blink.c): Added blink_once well

--- sandbox/blink.c
+++ sandbox/blink.c
@@ -38,7 +38,6 @@ void blink_init(void)
  */
 void blink_once(unsigned int delay_ms)
 {
-    /* TODO (Ejercicio B): implement on branch feat/blink-led */
     // LED_PIN on
     (void)delay_ms;
     // LED_PIN off
